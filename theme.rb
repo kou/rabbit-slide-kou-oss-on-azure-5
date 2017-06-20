c7d979569c1381574baefaf6ac089b6225e09543
Kouhei Sutou <kou@clear-code.com>
2017-06-20 21:03:26 +0900

    Import

---
+++ theme.rb
@@ -0,0 +1,5 @@
+@title_slide_title_font_size = @x_large_font_size
+@title_slide_subtitle_font_size = @small_font_size
+@title_slide_content_source_font_size = @xx_small_font_size
+
+include_theme("groonga")
